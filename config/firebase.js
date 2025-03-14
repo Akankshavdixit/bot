dedb3e21771b110c355b8b24f288c9fc1fc9fb94
Akankshavdixit <akankshavdixit@gmail.com>
2025-03-15 02:41:45 +0530

    adjusted .env

--- config/firebase.js
+++ config/firebase.js
@@ -3,16 +3,23 @@ const fs = require("fs");
 const {config} = require('dotenv');
 config()
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_FILE;
 
-if (!serviceAccountPath) {
-    throw new Error("Firebase service account file path not set.");
-}
-
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        type:process.env.TYPE,
+        project_id:process.env.PROJECTID ,
+        private_key_id:process.env.PRIVATEKEYID ,
+        private_key:process.env.PRIVATEKEY.replace(/\\n/g, '\n'),
+        client_email:process.env.CLIENTEMAIL,
+        client_id:process.env.CLIENTID,
+        auth_uri:process.env.AUTHURI,
+        token_uri:process.env.TOKENURI,
+        auth_provider_x509_cert_url:process.env.AUTHPROVIDERCERTURL,
+        client_x509_cert_url:process.env.CLIENTCERTURL,
+        universe_domain:process.env.UNIVERSEDOMAIN
+        
+    }),
     storageBucket: process.env.STORAGEBUCKET, 
 });
 
